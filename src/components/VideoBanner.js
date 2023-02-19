7ee9c1e98eade38328509d6a0a827cae2b3879d5
Jere <jirimicvl@outlook.com>
2023-02-19 18:46:30 +0200

    added bg grey and opacity

--- src/components/VideoBanner.js
+++ src/components/VideoBanner.js
@@ -3,7 +3,7 @@ import React from 'react';
 const VideoBanner = () => {
     return (
         <>
-            <div class="px-4 py-5 my-5 text-center">
+            <div class="px-4 py-5 my-5 text-center bg-opacity-25 bg-secondary">
                 <h1 class="display-5 fw-bold mb-5">Ret<span class="i">i</span>nk Content Solut<span class="i">i</span>on for Your Bus<span class="i">i</span>ness</h1>
                 <div class="col-lg-6 mx-auto">
                     <div class="ratio ratio-16x9">
