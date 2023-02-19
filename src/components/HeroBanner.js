d48a3fafede726a0df7568b8ee40cac9543fe214
Jere <jirimicvl@outlook.com>
2023-02-19 21:57:34 +0200

    before submitting to retink

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -3,14 +3,14 @@ import React from 'react';
 const HeroBanner = () => {
     return (
         <>
-            <div class="container col-xl-10 col-xxl-8 px-2 py-3">
+            <div class="container col-xl-10 col-xxl-8 py-3">
                 <div class="row align-items-center g-lg-5 py-3">
                     <div class="col-md-6 col-lg-8 text-center text-lg-start">
-                        <h1 class="display-4 fw-bold lh-1 mb-3">Get Advanced AI</h1>
-                        <h1 class="display-4 fw-bold lh-1 mb-3 ">+ Expert Created <span class='retink_red_font animated'></span></h1>
+                        <h1 class="display-4 fw-bold lh-1 mb-3">Get Advanced AI <span class='retink_red_font'>+</span></h1>
+                        <h1 class="display-4 fw-bold lh-1 mb-3 ">Expert Created <span class='retink_red_font animated'></span></h1>
                         <p class="col-lg-10 fs-5">Boost your sales <span class="retink_red_font">10x faster</span> with content customized by our unique partnership of <span class="retink_red_font">human creativity and AI optimization</span></p>
                     </div>
-                    <div class="col-md-6 col-lg-4">
+                    <div class="col-md-4 col-lg-4">
                         <img src="https://retink.io/Retink-avatar.png" alt="retink_mascot" width="95%" className='floating'/>
                     </div>
                 </div>
--- src/components/ServicesCardLayout.js
+++ src/components/ServicesCardLayout.js
@@ -10,9 +10,8 @@ const ServicesCardLayout = () => {
                             <img src="https://retink.io/istockphoto-655926676-612x612.jpg" class="border" alt="retink_logos" style={{  borderRadius: "25px"}}/>
                             <div class="card-body">
                                 <h3 class="card-title">Logos</h3>
-                                <p class="card-text mt-4">
-                                Create a distinct logo designed to express and represent your brand identity.
-                                </p>
+                                <hr />
+                                <p class="card-text mt-4">Create a distinct logo designed to express and represent your brand identity.</p>
                             </div>
                         </div>
                     </div>
@@ -21,9 +20,8 @@ const ServicesCardLayout = () => {
                         <img src="https://retink.io/andrew-neel-cckf4TsHAuw-unsplash.jpg" class="border" alt="blog_writing" style={{ borderRadius: "25px"}}/>
                         <div class="card-body">
                             <h3 class="card-title">Blog Writing</h3>
-                            <p class="card-text mt-4">
-                            Write SEO enriched blog posts as long or short articles on any topic of your choice.
-                            </p>
+                            <hr />
+                            <p class="card-text mt-4">Write SEO enriched blog posts as long or short articles on any topic of your choice.</p>
                         </div>
                         </div>
                     </div>
@@ -31,7 +29,8 @@ const ServicesCardLayout = () => {
                         <div class="card h-100 border-0">
                         <img src="https://retink.io/animation.jpg" class="border" alt="animated_videos " style={{ borderRadius: "25px"}} />
                         <div class="card-body">
-                            <h3 class="card-title">Animated Videos</h3>
+                            <h3 class="card-title ">Animated Videos</h3>
+                            <hr />
                             <p class="card-text mt-4">Bring animated characters to life to keep your viewers engaged and entertained.</p>
                         </div>
                         </div>
@@ -40,7 +39,8 @@ const ServicesCardLayout = () => {
                         <div class="card h-100 border-0">
                         <img src="https://retink.io/dlxmedia-hu-tp93kAQkHSQ-unsplash.jpg" class="border "  alt="product_demo" style={{ borderRadius: "25px"}}/>
                         <div class="card-body">
-                            <h3 class="card-title">Product Demo</h3>
+                            <h3 class="card-title ">Product Demo</h3>
+                            <hr />
                             <p class="card-text mt-4">Introduce your product to potential customers in a clear and creative video.</p>
                         </div>
                         </div>
@@ -50,6 +50,7 @@ const ServicesCardLayout = () => {
                         <img src="https://retink.io/luca-onniboni-4v9Kk01mEbY-unsplash.jpg" class="border"  alt="copywriting" style={{ borderRadius: "25px"}}/>
                         <div class="card-body">
                             <h3 class="card-title">Copywriting</h3>
+                            <hr />
                             <p class="card-text mt-4">Write compelling copies to boost your product, brand, service, or company.</p>
                         </div>
                         </div>
@@ -57,10 +58,11 @@ const ServicesCardLayout = () => {
 
                     <div class="col ">
                         <div class="card h-100 border-0">
-                        <img src="https://retink.io/dole777-gPet7PdxI1Q-unsplash.jpg" class="border"  alt="social_media" style={{ height: "97%", borderRadius: "25px"}}/>
+                        <img src="https://retink.io/dole777-gPet7PdxI1Q-unsplash.jpg" class="border"  alt="social_media" style={{ height: "68%", borderRadius: "25px"}}/>
                         <div class="card-body">
-                            <h3 class="card-title">Social Media Ads</h3>
-                            <p class="card-text">Boost sales and awareness with tailor made ads from experts.</p>
+                            <h3 class="card-title" >Social Media Ads</h3>
+                            <hr sx={{ opacity: "0"}} />
+                            <p class="card-text mt-4">Boost sales and awareness with tailor made ads from experts.</p>
                         </div>
                         </div>
                     </div>
