f3018d4b6a71fe5a465150ba53707d92646f933e
Jere <jirimicvl@outlook.com>
2023-02-19 12:43:10 +0200

    added slogan

---
+++ src/components/Slogan.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Slogan = () => {
+    return (
+        <>
+            <div class="px-4 py-4 my-4 text-center">
+                <h1 class="display-5 fw-bold">Transform Your Creat<span class="i">i</span>on Process</h1>
+                <div class="col-lg-6 mx-auto">
+                <p class="lead mb-4">With a new approach to ordering content, you can now stop juggling multiple documents and meetings and start publishing content faster and on demand.</p>
+                </div>
+            </div>
+
+            <div class="px-4 py-4 text-center">
+                <h3 class="display-5 fw-bold">Act<span class="i">i</span>vate Your Bus<span class="i">i</span>ness Growth W<span class="i">i</span>th Ret<span class="i">i</span>nk Content</h3>
+                <div class="col-lg-6 mx-auto">
+                <p class="lead mb-4">Save time and maintain your brand identity within your budget range within your budget range and access multiple content services like:</p>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Slogan;
