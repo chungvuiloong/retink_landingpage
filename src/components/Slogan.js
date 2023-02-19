2cfe273b8f3d6f7c683c524b215f8c9332763f3e
Jere <jirimicvl@outlook.com>
2023-02-19 18:46:46 +0200

    added bg grey and opacity

--- src/components/Slogan.js
+++ src/components/Slogan.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Slogan = () => {
     return (
         <>
-            <div class="px-4 py-4 my-4 text-center">
+            <div class="px-5 py-5 my-5 text-center bg-opacity-25 bg-secondary">
                 <h1 class="display-5 fw-bold">Transform Your Creat<span class="i">i</span>on Process</h1>
                 <div class="col-lg-6 mx-auto">
                 <p class="lead mb-4">With a new approach to ordering content, you can now stop juggling multiple documents and meetings and start publishing content faster and on demand.</p>
