5076af9bf5523d02dedcc808475b183f8f066f2f
Jere <jirimicvl@outlook.com>
2023-02-19 20:49:40 +0200

    added features and images

--- src/components/FeaturesCard.js
+++ src/components/FeaturesCard.js
@@ -4,16 +4,55 @@ import AI from '../assets/artificial-intelligence-image.png'
 const FeaturesCard = () => {
     return (
         <>
-            <div className="container">
-                <div class="bg-white card">
-                    <img src={AI} className="bg-white" alt="Card image cap" width="50px" sx={{}}/>
-                    <div class="card-body">
-                        <h4 class="card-title">Make AI-assisted Content Choices and Make it Human</h4>
-                        <p class="card-text">Access and order from an extensive catalogue in any language and on your budget's terms. If unsure, our AI guides you to the best human- content for your business.</p>
+            <section id="features" className="container bg-white">
+                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 text-center  d-flex justify-content-center justify-content-center-sm ">
+                    <div class="col mb-4">
+                        <div class="card text-center h-100 py-5 px-2"  style={{ borderRadius: "25px"}}>
+                        <img src="https://retink.io/artificial-intelligence-image.png" class="mx-auto" alt="ai_image" width="15%"/>
+                        <div class="card-body">
+                            <h5 class="card-title">Make AI-assisted Content Choices</h5>
+                            <p class="card-text mt-3">Access and order from an extensive catalogue in any language and on your budget's terms. If unsure, our AI guides you to the best.</p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col mb-4">
+                        <div class="card text-center h-100 py-5 px-2" style={{ borderRadius: "25px"}}>
+                        <img src="https://retink.io/brand-identity-image.png" class="mx-auto" alt="brand_identity_image" width="15%" />
+                        <div class="card-body">
+                            <h5 class="card-title">Upload and Maintain Your Brand Identity</h5>
+                            <p class="card-text mt-3">Add your preferences and brand assets to ensure the contents is consistent with your brand identity. No brand asset? We can make for you!</p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col mb-4">
+                        <div class="card text-center h-100 py-5 px-2" style={{ borderRadius: "25px"}}>
+                        <img src="https://retink.io/be-informed-image.png" class="mx-auto" alt="be_informed_image" width="15%" />
+                        <div class="card-body">
+                            <h5 class="card-title">Be Informed as We Create</h5>
+                            <p class="card-text mt-3">Your details are translated and sent to the AI, FP, or both (per your choice) as you relax and receive continuous updates..</p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col mb-4">
+                        <div class="card text-center h-100 py-5 px-2 " style={{ borderRadius: "25px"}}>
+                        <img src="https://retink.io/receive-and-review-image.png" class="mx-auto" alt="receive_and_review_image" width="15%" />
+                        <div class="card-body">
+                            <h5 class="card-title">Receive and Review</h5>
+                            <p class="card-text mt-3">Track your content metrics for improvement and content strategy.</p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col mb-4 text-center">
+                        <div class="card text-center h-100 py-5 px-2" style={{ borderRadius: "25px"}}>
+                        <img src="https://retink.io/publish-and-monitor-image.png" class="mx-auto" alt="publish_and_monitor_image" width="15%" />
+                        <div class="card-body">
+                            <h5 class="card-title">Publish and Monitor Your Contents' Progress</h5>
+                            <p class="card-text mt-3">The finished content is delivered and you can make corrections that we will effect to satisfy your requirements.</p>
+                        </div>
+                        </div>
                     </div>
                 </div>
-
-            </div>
+            </section>
         </>
     );
 };
